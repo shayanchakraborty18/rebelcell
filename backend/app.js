18f4110ba4ee217f68d1d6e6370b770f4380f070
Shayan Chakraborty <shayan@capitalnumbers.com>
2022-07-21 12:34:57 +0530

    push react files

--- backend/app.js
+++ backend/app.js
@@ -107,4 +107,14 @@ const adminRoutes = require('./routes/adminRoutes');
 app.use('/admin',  adminRoutes);
 
 
+if(process.env.NODE_ENV === 'PRODUCTION') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  })
+}
+
+
+
 module.exports = app;
